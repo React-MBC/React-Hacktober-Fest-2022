5577f9b4d02d0c0c94cf90486a6786479caaa852
aln6 <alanbjohn1212@gmail.com>
2022-10-17 07:36:11 +0530

    Added cars.py to detect cars from image

---
+++ Hack Here/Alan B John/cars.py
@@ -0,0 +1,29 @@
+from PIL import Image
+import cv2
+import numpy as np
+import requests
+
+cars_link = 'https://images.unsplash.com/photo-1589828155685-83225f7d91f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=710&q=80'
+image = Image.open(requests.get(cars_link, stream=True).raw)
+image = image.resize((450,250))
+image_arr = np.array(image)
+
+grey = cv2.cvtColor(image_arr,cv2.COLOR_BGR2GRAY)
+Image.fromarray(grey)
+blur = cv2.GaussianBlur(grey,(5,5),0)
+Image.fromarray(blur)
+dilated = cv2.dilate(blur,np.ones((3,3)))
+Image.fromarray(dilated)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
+closing = cv2.morphologyEx(dilated, cv2.MORPH_CLOSE, kernel)
+Image.fromarray(closing)
+car_cascade_src = 'cars.xml'
+car_cascade = cv2.CascadeClassifier(car_cascade_src)
+cars = car_cascade.detectMultiScale(closing, 1.1, 1)
+
+cnt = 0
+for (x,y,w,h) in cars:
+    cv2.rectangle(image_arr,(x,y),(x+w,y+h),(255,0,0),2)
+    cnt += 1
+print(cnt, " cars found")
+Image.fromarray(image_arr)
