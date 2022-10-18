e9f949f205ca6271c6d6771365aec617bed49133
pred07 <predatr921@gmail.com>
2022-10-18 22:32:18 +0530

    tressureisland.py

--- Hack Here/Brijith K Biju/bill.py
+++
@@ -1,8 +0,0 @@
-print("welcome to bill calculator")
-bill = float(input("total bill? $"))
-tip = int(input("tip percentage? 10,12or15:"))
-people = int(input("how many to split the bill?"))
-bill_with_tip = tip/100*bill+bill
-bill_with_person = bill_with_tip/people
-final_amount = round(bill_with_person,2)
-print(f"each should pay:${final_amount}")
--- Hack Here/Brijith K Biju/brand.py
+++
@@ -1,4 +0,0 @@
-print("welcome to brand generator")
-city = input("which city do you grow up in?")
-pet = input("name of your pet?")
-print("name is:" +city+""+pet)
