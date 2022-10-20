69cbf9a8fd7bd614e8072f46d6415de05b8fd7c0
al-312 <albinr312@gmail.com>
2022-10-20 21:50:00 +0530

    Added face.py

--- Hack Here/Albin Reji/face.py
+++ Hack Here/Albin Reji/face.py
@@ -1,20 +1,6 @@
-# OpenCV program to detect face in real time
-# import libraries of python OpenCV
-# where its functionality resides
+
 import cv2
-
-# load the required trained XML classifiers
-# https://github.com/Itseez/opencv/blob/master/
-# data/haarcascades/haarcascade_frontalface_default.xml
-# Trained XML classifiers describes some features of some
-# object we want to detect a cascade function is trained
-# from a lot of positive(faces) and negative(non-faces)
-# images.
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-# https://github.com/Itseez/opencv/blob/master
-# /data/haarcascades/haarcascade_eye.xml
-# Trained XML file for detecting eyes
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 # capture frames from a camera
