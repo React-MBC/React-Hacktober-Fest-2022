b2b6f307b75d0a79bc848b8d027006af4520a5ba
Albin-J <albinj7172@gmail.com>
2022-10-21 21:54:28 +0530

    Added audiocaptcha.py to create an audio captcha

--- Hack Here/Albin Joseph/audioccaptcha.py
+++
@@ -1,6 +0,0 @@
-from captcha.audio import AudioCaptcha
-audio = AudioCaptcha()
-captcha_text = "666"
-audio_data = audio.generate(captcha_text)
-audio_file = "audio"+captcha_text+'.wav'
-audio.write(captcha_text, audio_file)
