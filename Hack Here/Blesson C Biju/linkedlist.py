2630300fc01fbe877dccfe4aa3765827b5549721
Blezcherian <blezcherian07@gmail.com>
2022-10-25 23:43:20 +0530

    Added linkedlist.py

---
+++ Hack Here/Blesson C Biju/linkedlist.py
@@ -0,0 +1,14 @@
+class Node(object):
+# Constructor to initilize class variables
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+#get data
+def get_data(self):
+    return self.data
+# get next value
+def get_next(self):
+    return self.next_node
+# set next data
+def set_next(self, new_next):
+    self.next_node = new_next
