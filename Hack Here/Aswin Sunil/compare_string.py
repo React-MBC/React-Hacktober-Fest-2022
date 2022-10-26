055cdb19e4f21e40a2832cb7c45a12ce3fc9f08a
aswinsunil012 <aswinsunil565@gmail.com>
2022-10-26 22:10:44 +0530

    Added compare_string.py to compare two strings

---
+++ Hack Here/Aswin Sunil/compare_string.py
@@ -0,0 +1,10 @@
+list1 = input("Enter the values for list 1 : ") 
+list2 = input("Enter the values for list 2 : ") 
+  
+a = set(list1)  
+b = set(list2)  
+  
+if a == b:  
+    print("The list1 and list2 are equal")  
+else:  
+    print("The list1 and list2 are not equal") 
