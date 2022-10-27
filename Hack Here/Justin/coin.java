01a4032ceb8560c38ea4852b4f9b2d3b365f6042
Justin Eapen George <70130562+justinEapen@users.noreply.github.com>
2022-10-27 21:36:40 +0530

    coin.java

---
+++ Hack Here/Justin/coin.java
@@ -0,0 +1,59 @@
+public class coin {
+    long long int count(int coins[], int n, int sum) {
+
+ 
+
+        // code here.
+
+      long long int dp[sum+1][n+1];
+
+      
+
+      for(int i=0;i<=sum;i++)
+
+      for(int j=0;j<=n;j++)
+
+      dp[i][j]=0;
+
+      
+
+        for(int j=0;j<n;j++)
+
+        dp[0][j]=1;
+
+        
+
+        for(int i=1;i<sum;i++)
+
+        dp[i][0]=0;
+
+        
+
+        for(int i=1;i<=sum;i++)
+
+        {
+
+            for(int j=1;j<=n;j++)
+
+            {
+
+                dp[i][j]=dp[i][j-1];
+
+                
+
+                if(coins[j-1]<=i)
+
+                dp[i][j]+=dp[i-coins[j-1]][j];
+
+                
+
+            }
+
+        }
+
+        
+
+        return dp[sum][n];
+
+    }
+}
